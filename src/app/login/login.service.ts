046119e8429dfb87dc545829dc6265bc81badd16
zampieri <zampieri@leadlovers.com>
2020-11-10 00:57:32 -0300

    Logout

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -8,27 +8,27 @@ import { map, take } from 'rxjs/operators';
     providedIn: 'root'
 })
 export class LoginService {
-    constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) {}
 
-    public authenticate(email: string, password: string): Observable<string> {
-      return this._http.post<any>(`${environment.API_URL}/users/authenticate`,
-        { email, password }
-      ).pipe(
-        take(1),
-        map(response => {
-          localStorage.setItem('userId', JSON.stringify(response.user.id)),
-          localStorage.setItem('apiToken', JSON.stringify(response.token));
-          return response.token
-        })
-      );
-    }
+  public authenticate(email: string, password: string): Observable<string> {
+    return this._http.post<any>(`${environment.API_URL}/users/authenticate`,
+      { email, password }
+    ).pipe(
+      take(1),
+      map(response => {
+        localStorage.setItem('userId', JSON.stringify(response.user.id)),
+        localStorage.setItem('apiToken', JSON.stringify(response.token));
+        return response.token
+      })
+    );
+  }
 
-    public getApiToken(): string {
-        const token = JSON.parse(localStorage.getItem('apiToken'));
-        return token ? token : null;
-    }
+  public getApiToken(): string {
+    const token = JSON.parse(localStorage.getItem('apiToken'));
+    return token ? token : null;
+  }
 
-    public logout(): void {
-        localStorage.removeItem('apiToken');
-    }
+  public logout(): void {
+    localStorage.removeItem('apiToken');
+  }
 }
