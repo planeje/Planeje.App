7c95b0e3d9adfae2695cbd740aa5c533fb86811e
jvaps <fiirstcrw@hotmail.com>
2020-11-11 20:23:51 -0300

    hotfix

--- src/app/usual/models/activities.model.ts
+++ src/app/usual/models/activities.model.ts
@@ -1,7 +1,13 @@
+import { BankAccount } from './bank-account.model';
+import { Category } from './category.model';
+import { Transaction } from './transaction.model';
 export interface Activities {
     id: number;
     table: string;
     createdAt: Date;
     updatedAt: Date;
     deletedAt: Date;
+    transaction?: Transaction;
+    category?: Category;
+    bankAccount?: BankAccount;
   }
